d44ebc39deac5286ce614d97cc0c69178ceea65c
Shawn Wheeler <shawnrwheeler@gmail.com>
2019-05-25 21:09:44 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -35,6 +35,9 @@ def get_occupation(data, hometown)
     info.each do |contestant|
       if contestant["hometown"] == hometown
         return contestant["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
