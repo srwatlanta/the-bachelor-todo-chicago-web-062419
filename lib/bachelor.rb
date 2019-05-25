68e1e1efcb67e91af875deb36430ae876ad351ba
Shawn Wheeler <shawnrwheeler@gmail.com>
2019-05-25 21:07:45 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -19,7 +19,15 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  i = 0
+  data.each do |season_number, info|
+    info.each do |contestant|
+      if contestant["hometown"] == hometown
+        i += 1
+      end
+    end
+  end
+  return i
 end
 
 def get_occupation(data, hometown)
