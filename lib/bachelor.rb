0a80d6a9fd11c121418a3ee3df39880079ab10cf
Shawn Wheeler <shawnrwheeler@gmail.com>
2019-05-25 21:00:57 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -4,6 +4,8 @@ def get_first_name_of_season_winner(data, season)
   data[season].each do |contestant|
     if contestant["status"] = "Winner"
       return contestant["name"].split(' ').first
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
